8894e85b4e3c4b8ee65bbf8aea25dd3eecf37d05
argipap <argipapaefstathiou@gmail.com>
2018-04-09 20:48:59 +0300

    React forms and jtests

---
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import UsersList from '../UsersList';
+import renderer from 'react-test-renderer';
+
+const users = [
+    {
+        'active': true,
+        'email': 'argipapaefstathiou@gmail.com',
+        'id': 1,
+        'username': 'argi'
+    },
+    {
+        'active': true,
+        'email': 'jodi@gmail.com',
+        'id': 2,
+        'username': 'jodi'
+    }
+];
+
+test('UsersList renders properly', () => {
+    const wrapper = shallow(<UsersList users={users}/>);
+    const element = wrapper.find('h4');
+    expect(element.length).toBe(2);
+    expect(element.get(0).props.className).toBe('card card-body bg-light');
+    expect(element.get(0).props.children).toBe('argi');
+});
+
+test('UsersList renders a snapshot properly', () => {
+    const tree = renderer.create(<UsersList users={users}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
