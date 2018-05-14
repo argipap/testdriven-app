d6b835ef1bd57faa7307294c561e3e4f13f25fdb
argipap <argipapaefstathiou@gmail.com>
2018-05-15 00:15:58 +0300

    react update components

--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -1,31 +1,57 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import UsersList from '../UsersList';
-import renderer from 'react-test-renderer';
 
 const users = [
     {
         'active': true,
+        'admin': false,
         'email': 'argipapaefstathiou@gmail.com',
         'id': 1,
         'username': 'argi'
     },
     {
         'active': true,
+        'admin': false,
         'email': 'jodi@gmail.com',
         'id': 2,
         'username': 'jodi'
     }
-];
+]
 
 test('UsersList renders properly', () => {
     const wrapper = shallow(<UsersList users={users}/>);
-    const element = wrapper.find('h4');
-    expect(element.length).toBe(2);
-    expect(element.get(0).props.className).toBe('well');
-    expect(element.get(0).props.children).toBe('argi');
+    expect(wrapper.find('h1').get(0).props.children).toBe('All Users');
+    // table
+    const table = wrapper.find('Table');
+    expect(table.length).toBe(1);
+    expect(table.get(0).props.striped).toBe(true);
+    expect(table.get(0).props.bordered).toBe(true);
+    expect(table.get(0).props.condensed).toBe(true);
+    expect(table.get(0).props.hover).toBe(true);
+    // table head
+    expect(wrapper.find('thead').length).toBe(1);
+    const th = wrapper.find('th');
+    expect(th.length).toBe(5);
+    expect(th.get(0).props.children).toBe('User ID');
+    expect(th.get(1).props.children).toBe('Email');
+    expect(th.get(2).props.children).toBe('Username');
+    expect(th.get(3).props.children).toBe('Active');
+    expect(th.get(4).props.children).toBe('Admin');
+    // table body
+    expect(wrapper.find('tbody').length).toBe(1);
+    expect(wrapper.find('tbody > tr').length).toBe(2);
+    const td = wrapper.find('tbody > tr > td');
+    expect(td.length).toBe(10);
+    expect(td.get(0).props.children).toBe(1);
+    expect(td.get(1).props.children).toBe('argipapaefstathiou@gmail.com');
+    expect(td.get(2).props.children).toBe('argi');
+    expect(td.get(3).props.children).toBe('true');
+    expect(td.get(4).props.children).toBe('false');
 });
+
 
 test('UsersList renders a snapshot properly', () => {
     const tree = renderer.create(<UsersList users={users}/>).toJSON();
